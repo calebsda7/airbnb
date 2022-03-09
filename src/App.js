9e46d2b1a035b433e317952346633651c7a0c11c
calebsda7 <cey917@outlook.com>
2022-03-09 12:11:20 -0600

    updates

--- src/App.js
+++ src/App.js
@@ -1,33 +1,9 @@
 import "./App.css";
-import * as React from "react";
-import Home from "./HomePage";
-import NavBar from "./NavBar";
-import Login from "./LoginPage";
-import Footer from "./Footer";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Nearby from "./NearbyPage";
-import UpdateLogin from "./UpdateLogin";
+import React from "react";
+import HomePage from "./HomePage";
 
 const App = () => {
-	const [isModalOpen, setModalOpen] = React.useState(false);
-	const handleModalOpen = () => setModalOpen(true);
-	const handleModalClose = () => setModalOpen(false);
-
-	return (
-		<BrowserRouter>
-			<div>
-				<NavBar handleSetModalOpen={handleModalOpen} />
-				<Login open={isModalOpen} close={setModalOpen} />
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/Home" element={<Home />} />
-					<Route path="/Nearby" element={<Nearby />} />
-					<Route path="/Login" element={<UpdateLogin />} />
-				</Routes>
-				<Footer />
-			</div>
-		</BrowserRouter>
-	);
+  return <HomePage />;
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// import components
+import Home from "./HomePage";
+import Nearby from "./NearbyPage";
+import NavBar from "./NavBar";
+
+const rootElement = document.getElementById("root");
+
+render(
+  <BrowserRouter>
+    <NavBar />
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/nearby" element={<Nearby />} />
+      <Route
+        path="*"
+        element={
+          <main style={{ padding: "1rem" }}>
+            <p>There's nothing here!</p>
+          </main>
+        }
+      />
+    </Routes>
+  </BrowserRouter>,
+  rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
